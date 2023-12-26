7d4c08df315b3b24ce848dbb89c6b751e3d9d6b3
Nelsiemon <florent.roux@meritis.fr>
2023-12-26 23:48:22 +0100

    Add chip component

---
+++ src/components/base/roleChip.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import type { Role } from "$/shared/types/role.type";
+  export let label: Role;
+</script>
+
+<span class="rounded-full px-3 py-1 font-semibold bg-slate-200 text-slate-900 cursor-pointer text-sm">
+  {label}
+</span>
--- src/routes/pokedex/[pokeId]/+page.svelte
+++ src/routes/pokedex/[pokeId]/+page.svelte
@@ -2,10 +2,13 @@
   import type { PokemonSpecies } from "$/shared/types/pokemonSpecies.type";
   import Heading from "$/components/base/heading.svelte";
   import TypeChip from "$/components/base/typeChip.svelte";
+  import RoleChip from "$/components/base/roleChip.svelte";
+  import { Role } from "$/shared/types/role.type";
   export let data: PokemonSpecies;
 </script>
 
 <div class="flex flex-col">
   <Heading>{data.nameFr}</Heading>
   <p>Le type de {data.nameFr} est <TypeChip pokemonType={data.type1} /></p>
+  <p>Role : <RoleChip label={Role.ATTACKER} /></p>
 </div>
