4ef7bfb68b9c877e5bc7e4cb9a24d1351db33d10
Nelsiemon <florent.roux@meritis.fr>
2024-01-06 13:21:51 +0100

    Fix align in select multiple

--- src/components/base/selectMultiple.svelte
+++ src/components/base/selectMultiple.svelte
@@ -27,19 +27,19 @@
     {label}
   </span>
   <div class="relative">
-    <button on:click={() => isOpen = !isOpen} class="bg-pokeyellow text-gray-900 px-2 py-1 rounded-md font-semibold">
+    <button on:click={() => isOpen = !isOpen} class="bg-pokeyellow text-gray-900 px-2 py-1 rounded-md text-sm font-semibold">
       {selectedOptions.length} sélectionnés
     </button>
   
     {#if isOpen}
       <div
-        class="mt-2 w-full bg-white border border-gray-300 rounded-md shadow-lg flex flex-col absolute" 
+        class="mt-2 w-auto min-w-full bg-white border border-gray-300 rounded-md shadow-lg flex flex-col absolute" 
         use:clickOutside on:click_outside={() => { isOpen = false }}
       >
         {#each options as option, i (i)}
-          <div class="flex flex-row gap-1 px-2 py-1 cursor-pointer">
+          <div class="flex flex-row gap-1 px-2 py-1 cursor-pointer select-none">
             <input type="checkbox" id={`option ${i}`} on:change={(e) => toggleOption(e, option) } />
-            <label for={`option ${i}`} class="text-gray-700">{labelKey ? option[labelKey] : option}</label>
+            <label for={`option ${i}`} class="text-gray-700 whitespace-nowrap">{labelKey ? option[labelKey] : option}</label>
           </div>
         {/each}
       </div>
