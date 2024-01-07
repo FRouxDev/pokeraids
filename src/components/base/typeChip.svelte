b2fb68c38648c7a1f9fb0371554a4d27ab99d239
Nelsiemon <florent.roux@meritis.fr>
2024-01-07 12:18:59 +0100

    Update type images folder structures

--- src/components/base/typeChip.svelte
+++ src/components/base/typeChip.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import type { PokemonType } from "$/shared/types/pokemonType.type";
   export let pokemonType: PokemonType | undefined;
+  export let size: 'regular' | 'small' = 'small';
+  $: prefix = size === 'small' ? '80px-' : '';
 </script>
 
 {#if pokemonType}
   {@const alt = `Type ${pokemonType}`}
-  {@const src = `/types/80px-Miniature_Type_${pokemonType}_EV.png`}
+  {@const src = `/types/${size}/${prefix}Miniature_Type_${pokemonType}_EV.png`}
   <img {src} alt="{alt}" class="m-auto"/>
 {/if}
