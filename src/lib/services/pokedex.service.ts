384f7667fba85b39749ce3e78bb87212e06d3097
Nelsiemon <florent.roux@meritis.fr>
2024-01-11 12:00:14 +0100

    remove test console

--- src/lib/services/pokedex.service.ts
+++ src/lib/services/pokedex.service.ts
@@ -9,6 +9,5 @@ export const findPokemonById = async (pokemonId: string): Promise<PokemonSpecies
 
 export const getAllPokemon = async (): Promise<PokemonSpecies[]> => {
   const pokemonList = await PokemonSpeciesModel.find().populate('abilities').lean();
-  console.log(pokemonList);
   return structuredClone(pokemonList);
 };
